736dfec5b72276974fab40456a76f1cfaaaff434
Isaac Trim <itrim@umich.edu>
2025-04-22 13:55:33 -0400

    Attempt 1

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,67 @@
 #include<iostream>
 #include "Game.h"
+using namespace std;
 
-// complete the implementation of the Game class here
+Game::Game() {
+    playGame();
+}
+void Game::printMatches(vector<int>playerPicks,vector<int>kenoNumbers) {
+    m_matches.clear();
+    int j = 0;
+    for (int i = 0; i < 20;) {
+        if(j == 11) {
+            i=100;
+        }
+        else if (kenoNumbers[i] == playerPicks[j]) {
+            m_matches.push_back(kenoNumbers[i]);
+            i++;
+            j++;
+        }
+        else if(kenoNumbers[i] > playerPicks[j]) {
+            j++;
+        }
+        else {
+            i++;
+        }
+    }
+    for (auto i:m_matches) {
+        std::cout << i << " ";
+    }
+}
+void Game::displayResults() {
+
+}
+void Game::playGame() {
+    m_matches.clear();
+    m_player.inputPlayerNumbers();
+    m_keno.generateNumbers();
+    cout << endl << endl << "----------------------------------Results----------------------------------" << endl;
+    cout << "Keno Numbers: ";
+    m_keno.printKenoNumbers();
+    cout << endl;
+    cout << "Player Picks: ";
+    m_player.printPlayerNumbers();
+    cout << endl;
+    cout << "You matched: ";
+    printMatches(m_player.getPlayerNumbers(), m_keno.getKenoNumbers());
+    int prize = 0;
+    if(m_matches.size()==5) {
+        prize = 5;
+    }
+    else if(m_matches.size()==6) {
+        prize = 10;
+    }
+    else if(m_matches.size()==7) {
+        prize = 50;
+    }
+    else if(m_matches.size()==8) {
+        prize = 250;
+    }
+    else if(m_matches.size()==9) {
+        prize = 2500;
+    }
+    else if(m_matches.size()==10) {
+        prize = 25000;
+    }
+    cout <<  endl << "Matching " << m_matches.size() << " of 20 numbers wins $" << prize;
+}
